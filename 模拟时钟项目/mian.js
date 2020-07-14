4caadd383e0df14852a4fbea3e8d1a5cd1d1ddbe
jingdd-boop <1736929925@qq.com>
2020-07-14 15:20:09 +0800

    6

--- 模拟时钟项目/mian.js
+++ 模拟时钟项目/mian.js
@@ -91,3 +91,4 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
 setTime();
 setInterval(setTime, 1000);
 
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
